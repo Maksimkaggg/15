dff3a3c8e062c244441bab835bc614109b8fbbde
Максик <sharapovm505@gmail.com>
2023-01-26 07:52:34 +0300

    15 на все

---
+++ main.py
@@ -0,0 +1,24 @@
+# функция делимость
+'''def f(x,a):
+    return (a % 3 == 0) and ((220 % x == 0) <= ((a % x != 0) <= (550 % x != 0)))
+
+for a in range(1, 1001):
+    if all(f(x, a) == 1 for x in range(1, 10000)):
+        print(a)'''
+
+# функция график
+'''def f(x, y, a):
+    return ((x - 2*y) < 3*a) or (2*y > x) or (3*x > 50) #выражение в ()
+
+for a in range(1, 1000):
+    if all(f(x, y, a) == 1 for x in range(1, 1000) for y in range(1, 1000)):
+        print(a)'''
+
+
+#фунция коньюнкция
+'''def f(x, a):
+    return (x & 107 == 0) <= ((x & 55 != 0) <= (x & a != 0))
+
+for a in range(1, 1000):
+    if all(f(x, a) == 1 for x in range(1, 1000)):
+        print(a)'''
